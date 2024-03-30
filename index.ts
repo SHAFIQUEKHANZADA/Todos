b77b8eb2991da8668c0ce294dc719134571912e3
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-30 09:42:07 +0500

    add todos

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let todos = []
+let condition = true
+
+while (condition ) {
+let task = await inquirer.prompt([{
+    name: "addtask",
+    type: "input",
+    message: chalk.italic.magentaBright("What do you want to add in your Todos ?")
+},
+{
+    name: "addMore",
+    type: "confirm",
+    message: chalk.bold.cyanBright( "Do you want to add more ?"),
+    default: "false"
+}
+])
+todos.push(task.addtask)
+condition = task.addMore
+console.log(todos);
+}
+ 
