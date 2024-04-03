aefb660af1d0b004f55103aa9d303ebaffff0840
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-04-03 16:14:29 +0500

    updated

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-import chalk from "chalk";
 let todos = [];
-let condition = true;
-while (condition) {
-    let task = await inquirer.prompt([{
-            name: "addtask",
-            type: "input",
-            message: chalk.italic.magentaBright("What do you want to add in your Todos ?")
-        },
-        {
-            name: "addMore",
-            type: "confirm",
-            message: chalk.bold.cyanBright("Do you want to add more ?"),
-            default: "false"
+// let condition = true;
+async function creat(todos) {
+    do {
+        let welcom = "Welcom to the todolist";
+        let oprate = await inquirer.prompt({
+            name: "op",
+            type: "list",
+            message: "What you want to do ?",
+            choices: ["Add", "View", "Update", "Delete"],
+        });
+        if (oprate.op === "Add") {
+            let additem = await inquirer.prompt({
+                name: "adit",
+                type: "input",
+                message: "Please Add Item!",
+            });
+            todos.push(additem.adit);
+            console.log(todos);
         }
-    ]);
-    todos.push(task.addtask);
-    condition = task.addMore;
-    console.log(todos);
+        if (oprate.op === "View") {
+            console.log(todos);
+        }
+        if (oprate.op === "Update") {
+            let Updat = await inquirer.prompt([
+                {
+                    name: "u1",
+                    type: "list",
+                    message: "Select item to update",
+                    choices: todos,
+                }
+            ]);
+            let Updat2 = await inquirer.prompt([
+                {
+                    name: "u2",
+                    type: "input",
+                    message: "Update Item",
+                }
+            ]);
+            let newUp = todos.filter(val => val != Updat.u1);
+            todos = [...todos, Updat2.u2];
+        }
+        if (oprate.op === "Delete") {
+            let remove = await inquirer.prompt({
+                name: "rem",
+                type: "list",
+                message: "Select an item to delete",
+                choices: todos
+            });
+        }
+    } while (true);
 }
+creat(todos);
